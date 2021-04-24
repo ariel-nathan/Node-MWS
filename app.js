a5a3bdc9b43b33bf8d10460a08538909985120f9
Ariel Nathan <nathanariel17@gmail.com>
2021-04-24 04:51:29 -0400

    port update

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ import express from "express";
 
 dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Running on Port: ${PORT}`);
+});
+
 cron.schedule("* * * * *", () => {
   console.log("running every minute");
 });
